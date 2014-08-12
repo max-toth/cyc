38eb55a8226eed6a2180924f863cd31115b7d317
mtolstykh <maxim.v.tolstyh@gmail.com>
2014-08-12 23:44:02 +0400

    rotate pyramid

--- src/main/java/JOGLQuad.java
+++ src/main/java/JOGLQuad.java
@@ -1,6 +1,9 @@
+import com.jogamp.opengl.util.FPSAnimator;
+
 import javax.media.opengl.*;
 import javax.media.opengl.awt.GLCanvas;
 import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.GLUquadric;
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -17,69 +20,102 @@ public class JOGLQuad {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
-        GLCapabilities glcapabilities = new GLCapabilities( glp );
-        final GLCanvas glcanvas = new GLCanvas( glcapabilities );
+        GLCapabilities glcapabilities = new GLCapabilities(glp);
+        final GLCanvas glcanvas = new GLCanvas(glcapabilities);
 
-        glcanvas.addGLEventListener( new GLEventListener() {
+        final FPSAnimator animator = new FPSAnimator(glcanvas, 60, true);
+        glcanvas.addGLEventListener(new GLEventListener() {
 
             @Override
-            public void reshape( GLAutoDrawable glautodrawable, int x, int y, int width, int height ) {
+            public void reshape(GLAutoDrawable glautodrawable, int x, int y, int width, int height) {
                 setup(glautodrawable.getGL().getGL2(), width, height);
             }
 
             @Override
-            public void init( GLAutoDrawable glautodrawable ) {
+            public void init(GLAutoDrawable glautodrawable) {
             }
 
             @Override
-            public void dispose( GLAutoDrawable glautodrawable ) {
+            public void dispose(GLAutoDrawable glautodrawable) {
             }
 
             @Override
-            public void display( GLAutoDrawable glautodrawable ) {
-                render( glautodrawable.getGL().getGL2(), glautodrawable.getSurfaceWidth(), glautodrawable.getSurfaceHeight() );
+            public void display(GLAutoDrawable glautodrawable) {
+                render(glautodrawable.getGL().getGL2(), glautodrawable.getSurfaceWidth(), glautodrawable.getSurfaceHeight());
+                rtri += 0.2f;
             }
         });
 
-        frame.add( glcanvas );
-        frame.addWindowListener( new WindowAdapter() {
-            public void windowClosing( WindowEvent windowevent ) {
-                frame.remove( glcanvas );
-                frame.dispose();
-                System.exit( 0 );
+        frame.add(glcanvas);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowevent) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        if (animator.isStarted()) animator.stop();
+                        System.exit(0);
+                    }
+                }.start();
             }
         });
 
-        frame.setSize( 640, 480 );
-        frame.setVisible( true );
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+        animator.start();
     }
 
-    protected static void setup( GL2 gl2, int width, int height ) {
-        gl2.glMatrixMode( GL2.GL_PROJECTION );
+    protected static void setup(GL2 gl2, int width, int height) {
+        gl2.glViewport(0, 0, width, height);
+        gl2.glMatrixMode(GL2.GL_PROJECTION);
         gl2.glLoadIdentity();
-
-        // coordinate system origin at lower left with width and height same as the window
-        GLU glu = new GLU();
-        glu.gluOrtho2D( 0.0f, width, 0.0f, height );
-
-        gl2.glMatrixMode( GL2.GL_MODELVIEW );
+        glu.gluPerspective(45.0f, width / height, 0.1f, 100.0f);
+        gl2.glMatrixMode(GL2.GL_MODELVIEW);
+//        glu.gluLookAt(10.0f, 10.0f, 10.0f, 0.0f, 0.0f, 0.0f, 5.0f, 5.0f, 5.0f);
+        gl2.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         gl2.glLoadIdentity();
-
-        gl2.glViewport( 0, 0, width, height );
+        gl2.glTranslatef(-0.5f, -0.5f, -6.0f);
     }
 
-    protected static void render( GL2 gl2, int width, int height ) {
-        gl2.glClear( GL.GL_COLOR_BUFFER_BIT );
+    static GLU glu = new GLU();
+    static float rtri;
 
-        // draw a triangle filling the window
-        gl2.glLoadIdentity();
-        gl2.glBegin( GL.GL_TRIANGLES );
-        gl2.glColor3f( 1, 0, 0 );
-        gl2.glVertex2f( 0, 0 );
-        gl2.glColor3f( 0, 1, 0 );
-        gl2.glVertex2f( width, 0 );
-        gl2.glColor3f( 0, 0, 1 );
-        gl2.glVertex2f( width / 2, height );
+    protected static void render(GL2 gl2, int width, int height) {
+        gl2.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        gl2.glLoadIdentity();                 // reset the model-view matrix
+        gl2.glTranslatef(-0.5f, -0.5f, -6.0f);
+        gl2.glRotatef(rtri,  -1.0f, 1.0f, 1.0f);
+        gl2.glBegin(GL.GL_TRIANGLES);
+        gl2.glColor3f(1.0f, 0.0f, 0.0f); // Red
+        gl2.glVertex3f(0.0f, 1.0f, 0.0f);
+        gl2.glColor3f(0.0f, 1.0f, 0.0f); // Green
+        gl2.glVertex3f(-1.0f, -1.0f, 1.0f);
+        gl2.glColor3f(0.0f, 0.0f, 1.0f); // Blue
+        gl2.glVertex3f(1.0f, -1.0f, 1.0f);
+
+        // Right-face triangle
+        gl2.glColor3f(1.0f, 0.0f, 0.0f); // Red
+        gl2.glVertex3f(0.0f, 1.0f, 0.0f);
+        gl2.glColor3f(0.0f, 0.0f, 1.0f); // Blue
+        gl2.glVertex3f(1.0f, -1.0f, 1.0f);
+        gl2.glColor3f(0.0f, 1.0f, 0.0f); // Green
+        gl2.glVertex3f(1.0f, -1.0f, -1.0f);
+
+        // Back-face triangle
+        gl2.glColor3f(1.0f, 0.0f, 0.0f); // Red
+        gl2.glVertex3f(0.0f, 1.0f, 0.0f);
+        gl2.glColor3f(0.0f, 1.0f, 0.0f); // Green
+        gl2.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl2.glColor3f(0.0f, 0.0f, 1.0f); // Blue
+        gl2.glVertex3f(-1.0f, -1.0f, -1.0f);
+
+        // Left-face triangle
+        gl2.glColor3f(1.0f, 0.0f, 0.0f); // Red
+        gl2.glVertex3f(0.0f, 1.0f, 0.0f);
+        gl2.glColor3f(0.0f, 0.0f, 1.0f); // Blue
+        gl2.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl2.glColor3f(0.0f, 1.0f, 0.0f); // Green
+        gl2.glVertex3f(-1.0f, -1.0f, 1.0f);
         gl2.glEnd();
     }
+
 }
