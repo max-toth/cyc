dd2f5456d2b5d28b1132a8661421b1ea3669f989
mtolstykh <maxim.v.tolstyh@gmail.com>
2014-08-12 18:39:51 +0400

    opengl rendering triangle

--- src/main/java/JOGLQuad.java
+++ src/main/java/JOGLQuad.java
@@ -1,5 +1,9 @@
-import javax.media.opengl.GLProfile;
+import javax.media.opengl.*;
+import javax.media.opengl.awt.GLCanvas;
+import javax.media.opengl.glu.GLU;
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class JOGLQuad {
 
@@ -9,9 +13,73 @@ public class JOGLQuad {
         GLProfile glp = GLProfile.getMaxFixedFunc(true);
 
         final JFrame frame = new JFrame("Jogl Quad drawing");
-        frame.setSize(200, 75);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        GLCapabilities glcapabilities = new GLCapabilities( glp );
+        final GLCanvas glcanvas = new GLCanvas( glcapabilities );
+
+        glcanvas.addGLEventListener( new GLEventListener() {
+
+            @Override
+            public void reshape( GLAutoDrawable glautodrawable, int x, int y, int width, int height ) {
+                setup(glautodrawable.getGL().getGL2(), width, height);
+            }
+
+            @Override
+            public void init( GLAutoDrawable glautodrawable ) {
+            }
+
+            @Override
+            public void dispose( GLAutoDrawable glautodrawable ) {
+            }
+
+            @Override
+            public void display( GLAutoDrawable glautodrawable ) {
+                render( glautodrawable.getGL().getGL2(), glautodrawable.getSurfaceWidth(), glautodrawable.getSurfaceHeight() );
+            }
+        });
+
+        frame.add( glcanvas );
+        frame.addWindowListener( new WindowAdapter() {
+            public void windowClosing( WindowEvent windowevent ) {
+                frame.remove( glcanvas );
+                frame.dispose();
+                System.exit( 0 );
+            }
+        });
+
+        frame.setSize( 640, 480 );
+        frame.setVisible( true );
+    }
+
+    protected static void setup( GL2 gl2, int width, int height ) {
+        gl2.glMatrixMode( GL2.GL_PROJECTION );
+        gl2.glLoadIdentity();
+
+        // coordinate system origin at lower left with width and height same as the window
+        GLU glu = new GLU();
+        glu.gluOrtho2D( 0.0f, width, 0.0f, height );
+
+        gl2.glMatrixMode( GL2.GL_MODELVIEW );
+        gl2.glLoadIdentity();
+
+        gl2.glViewport( 0, 0, width, height );
+    }
+
+    protected static void render( GL2 gl2, int width, int height ) {
+        gl2.glClear( GL.GL_COLOR_BUFFER_BIT );
+
+        // draw a triangle filling the window
+        gl2.glLoadIdentity();
+        gl2.glBegin( GL.GL_TRIANGLES );
+        gl2.glColor3f( 1, 0, 0 );
+        gl2.glVertex2f( 0, 0 );
+        gl2.glColor3f( 0, 1, 0 );
+        gl2.glVertex2f( width, 0 );
+        gl2.glColor3f( 0, 0, 1 );
+        gl2.glVertex2f( width / 2, height );
+        gl2.glEnd();
     }
 }
